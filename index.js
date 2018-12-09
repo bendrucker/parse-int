e3c43dde0853751ad1470b9d29893952c75ff24b
Andrew <fedyk72@gmail.com>
2018-12-09 14:30:29 +0100

    Speed up parse-int

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
 var isInteger = require('is-integer')
+var isIntegerRegex = /^-?\d+$/
 
 module.exports = function parseIntStrict (integer) {
   if (typeof integer === 'number') {
     return isInteger(integer) ? integer : undefined
   }
   if (typeof integer === 'string') {
-    return /^-?\d+$/.test(integer) ? parseInt(integer, 10) : undefined
+    return isIntegerRegex.test(integer) ? parseInt(integer, 10) : undefined
   }
 }
